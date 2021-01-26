49ba195a29372a70f9d0b92c54cb13b1ea9ebf32
natekcroteau <natekcroteau@gmail.com>
2021-01-26 11:40:42 -0700

    seed backend

--- deals_backend/db/seeds.rb
+++ deals_backend/db/seeds.rb
@@ -1,11 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-require 'rest-client'
+Store.destroy_all
+User.destroy_all
 
-stores =  RestClient.get 'https://www.cheapshark.com/api/1.0/stores'
+steam = Store.create( store_id: "1", name: "Steam")
+amazon = Store.create( store_id: "4", name: "Amazon")
+gameStop = Store.create( store_id: "5", name: "GameStop")
+epic_games_store = Store.create( store_id: "25", name: "Epic Games Store")
+humble_store = Store.create( store_id: "11", name: "Humble Store")
+
+User.create(username: "user1", password: "password", store: steam)
+
+
